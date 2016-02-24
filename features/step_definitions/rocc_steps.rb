db89df5315e12298ccf3909ef3ec001ed004daa0
Thilo Fischer <thilo-fischer@gmx.de>
2016-02-24 12:25:26 +0100

    cleanup rocc_steps definition

--- features/step_definitions/rocc_steps.rb
+++ features/step_definitions/rocc_steps.rb
@@ -12,62 +12,62 @@ end
 
 Then /^the output should look as specified by "([^"]*)"$/ do |basepath|
 
-  @basepath = File.join(TESTDATA_DIR, 'expect', basepath)
+  basepath = File.join(TESTDATA_DIR, 'expect', basepath)
 
-  filename_out = @basepath + '.stdout'
+  filename_out = basepath + '.stdout'
 
-  filecontent = IO.read(filename_out)
-  parts = filecontent.split('///')
-  if parts.length == 1
+  expect_out = IO.read(filename_out)
+  out_parts = expect_out.split('///')
+  if out_parts.length == 1
     # use string match if no regular expression specified in
     # expectiation file => better diff display if test fails
-    @expect_out = filecontent
+    
     # XXX_R is there a better syntax? e.g. with `step' instead of
     # `steps'?
     steps %Q{
     Then the output should contain exactly:
 """
-#{@expect_out}
+#{expect_out}
 """
     }
   else
     literals = []
     regexps  = []
-    parts.each_slice(2) do |slc|
+    out_parts.each_slice(2) do |slc|
       literals << slc[0]
       regexps  << slc[1]
     end
-    @expect_out = ''
+    regexp_source = ''
     literals.each_with_index do |l, idx|
-      @expect_out << Regexp.escape(l)
-      @expect_out << regexps[idx] if regexps[idx]
+      regexp_source << Regexp.escape(l)
+      regexp_source << regexps[idx] if regexps[idx]
     end
-    #warn "AAAA #{@expect_out}"
+    #warn "AAAA #{regexp_source}"
     
     # XXX_R is there a better syntax? e.g. with `step' instead of
     # `steps'?
     steps %Q{
     Then the output should match:
 """
-#{@expect_out}
+#{regexp_source}
 """
     }
   end
   
-  filename_err = @basepath + '.stderr'
+  filename_err = basepath + '.stderr'
   if File.exist?(filename_err)
-    @expect_err = IO.read(filename_err)
+    expect_err = IO.read(filename_err)
   else
-    @expect_err = nil
+    expect_err = nil
   end
   
-  if @expect_err
+  if expect_err
     # XXX_R is there a better syntax? e.g. with `step' instead of
     # `steps'?
     steps %Q{
       And the stderr should contain exactly:
 """
-#{@expect_err}
+#{expect_err}
 """
   }
   end
